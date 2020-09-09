092cf5c68539640db06155f94a6dbf60dba7d5f1
sameep <sameep.v@ahduni.edu.in>
2020-09-09 17:27:27 +0530

    Second commit

---
+++ src/databaseprojectdsa/Database.java
@@ -0,0 +1,105 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseprojectdsa;
+import java.io.*;
+/**
+ *
+ * @author Sameep
+ */
+public class Database {
+private RandomAccessFile database;
+private String fName = new String();
+java.util.Scanner kb = new java.util.Scanner(System.in);
+Database(){
+}
+
+ void add(DbObject d)throws IOException {
+	 database = new RandomAccessFile(fName, "rw");
+	 database.seek(database.length());
+	 d.writeToFile(database);
+	 database.close();
+ }
+ void modify(DbObject d)throws IOException {
+	 DbObject[] tmp = new DbObject[1];
+	 d.copy(tmp);
+	 database = new RandomAccessFile(fName, "rw");
+	 while(database.getFilePointer()< database.length()) {
+		 tmp[0].readFromFile(database);
+		 if(tmp[0].equals(d)) {
+			 tmp[0].readFromConsole();
+			 database.seek(database.getFilePointer() - d.size());
+			 tmp[0].writeToFile(database);
+			 database.close();
+			 return;
+		 }
+	 }
+ }
+ boolean find(DbObject d)throws IOException {
+//	DbObject tmp = new DbObject(); cannot initialize abstract object!!!
+	 DbObject[] tmp = new DbObject[1];
+	 d.copy(tmp);
+	 database = new RandomAccessFile(fName, "r");
+	 while(database.getFilePointer()<database.length()) {
+		 tmp[0].readFromFile(database);
+		 if(tmp[0].equals(d)) {
+			 database.close();
+			 return true;
+		 }
+	 }
+	 database.close();
+	return false;
+}
+ void printDb(DbObject d)throws IOException {
+	
+	 database = new RandomAccessFile(fName, "r");
+	 while(database.getFilePointer()< database.length()) {
+		 d.readFromFile(database);
+		 d.writeLegibly();
+		 System.out.println();
+	 }
+	database.close();
+}
+
+public void run(DbObject rec) throws IOException {
+    String option;
+    System.out.print("File name: ");
+    fName = kb.next();
+    System.out.println("1. Add 2. Find 3. Modify a record; 4. Exit");
+    System.out.print("Enter an option: ");
+    option = kb.next();
+    while (true) {
+        if (option.charAt(0) == '1') {
+             rec.readFromConsole();
+             add(rec);
+        }
+        else if (option.charAt(0) == '2') {
+             rec.readKey();
+             System.out.print("The record is ");
+             if (find(rec) == false)
+                 System.out.print("not ");
+             System.out.println("in the database");
+        }
+        else if (option.charAt(0) == '3') {
+             rec.readKey();
+             modify(rec);
+        }
+        else if (option.charAt(0) != '4')
+             System.out.println("Wrong option");
+        else return;
+        printDb(rec);
+        System.out.println("1. Add 2. Find 3. Modify a record; 4. Exit");
+        System.out.print("Enter an option: ");
+        option = kb.next();
+    }
+}
+
+
+static public void main(String[] args) throws IOException {
+//create database and run it
+	Database trees = new Database();
+	trees.run(new Tree());
+}
+}
---
+++ src/databaseprojectdsa/DbObject.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package databaseprojectdsa;
+import java.io.*;
+
+/**
+ *
+ * @author Sameep
+ */
+abstract class DbObject {
+
+	 java.util.Scanner kb = new java.util.Scanner(System.in);
+	    public void writeString(String s, RandomAccessFile out) throws IOException {
+	        for (int i = 0; i < s.length(); i++)
+	            out.writeChar(s.charAt(i));
+	    }
+	    public String readString(int len, RandomAccessFile in) throws IOException {
+	        StringBuffer s = new StringBuffer(len);
+	        for (int i = 0; i < len; i++)
+	            s.append(in.readChar());
+	        return s.toString();
+	    }
+	    abstract public void writeToFile(RandomAccessFile out) throws IOException;
+	    abstract public void readFromFile(RandomAccessFile in) throws IOException;
+	    abstract public void readFromConsole();
+	    abstract public void writeLegibly();
+	    abstract public void readKey();
+	    abstract public void copy(DbObject[] db);
+	    abstract public int size();
+
+}
